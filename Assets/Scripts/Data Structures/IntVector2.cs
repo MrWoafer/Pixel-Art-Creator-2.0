833f195560af174459f845423002b0e1e0ff9448
MrWoafer <woafer1000@gmail.com>
2024-11-18 22:14:29 +0000

    Re-add proof comment

--- Assets/Scripts/Data Structures/IntVector2.cs
+++ Assets/Scripts/Data Structures/IntVector2.cs
@@ -337,6 +337,12 @@ namespace PAC.DataStructures
         /// </summary>
         public static bool AreColinear(IntVector2 a, IntVector2 b)
         {
+            // Proof that being colinear in the sense of real vectors is equivalent to there being an IntVector2 dividing both of them:
+            //      The case where a = 0 or b = 0 is trivial, so assume a and b are non-zero. Without loss of generality, let a, b be in the top-right quadrant. Let c be the smallest positive
+            //      integer point on the line. It is enough to the prove that c divides a (as then, by symmetry, c divides b; and by definition of Simplify() and minimality of c, c = Simplify(a),
+            //      Simplify(b)). So suppose c did not divide a. Let d be the greatest multiple of c less than a (note c < a). Note d is on the line too, and hence so is a - d, which is an integer
+            //      vector. But, by definition of d, 0 < a - d < c, contradicting minimality of c!
+
             // This is a rearrangement of a.y / a.x == b.y / b.x (comparing gradients) which avoids floats and division by 0.
             return a.y * b.x == b.y * a.x;
         }
