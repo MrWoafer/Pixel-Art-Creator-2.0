a7525006faf43d98c8c7cce845a6a667beace47c
MrWoafer <woafer1000@gmail.com>
2024-12-05 23:12:44 +0000

    Add comments

--- Assets/Scripts/Exceptions/UnreachableException.cs
+++ Assets/Scripts/Exceptions/UnreachableException.cs
@@ -5,5 +5,12 @@ using System;
 /// </summary>
 public class UnreachableException : Exception
 {
+    /// <summary>
+    /// Indicates that logically this code should not be reachable.
+    /// </summary>
+    public UnreachableException() : base("Reached code that should be unreachable.") { }
+    /// <summary>
+    /// Indicates that logically this code should not be reachable.
+    /// </summary>
     public UnreachableException(string message) : base(message) { }
 }
