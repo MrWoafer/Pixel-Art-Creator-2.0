0de7efd5953e28811bfdee9dc814375625845988
MrWoafer <woafer1000@gmail.com>
2024-12-15 12:28:29 +0000

    Add IEnumerableExtensions.Random(Random, IntRange)

--- Assets/Scripts/Extensions/Data Structures/IntRangeExtensions.cs
+++ Assets/Scripts/Extensions/Data Structures/IntRangeExtensions.cs
@@ -14,5 +14,16 @@ namespace PAC.Extensions
         /// </summary>
         public static int Next(this Random rng, IntRange range) =>
             range.isEmpty ? throw new ArgumentException("The given range is empty.", nameof(range)) : rng.Next(range.minElement, range.maxElement + 1);
+
+        /// <summary>
+        /// Creates an infinite random sequence.
+        /// </summary>
+        public static IEnumerable<int> Random(Random rng, IntRange range)
+        {
+            while (true)
+            {
+                yield return rng.Next(range);
+            }
+        }
     }
 }
