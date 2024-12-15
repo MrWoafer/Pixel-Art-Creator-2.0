08f0a9ef7a13646555581f0e3905293af007ac9a
MrWoafer <woafer1000@gmail.com>
2024-12-15 04:36:53 +0000

    Add UndefinedOperationException

---
+++ Assets/Scripts/Exceptions/UndefinedOperationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PAC.Exceptions
+{
+    /// <summary>
+    /// Indicates that an operation attempting to be performed is undefined.
+    /// </summary>
+    public class UndefinedOperationException : InvalidOperationException
+    {
+        /// <summary>
+        /// Indicates that an operation attempting to be performed is undefined.
+        /// </summary>
+        public UndefinedOperationException(string message) : base(message) { }
+    }
+}
