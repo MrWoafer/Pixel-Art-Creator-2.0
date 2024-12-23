f04d321527e91ba5be2675b9dd802cbf04816ec6
MrWoafer <woafer1000@gmail.com>
2024-12-23 23:55:55 +0000

    Add custom 'Assets > Create' menu item for creating completely empty .cs file

--- Assets/Editor/CreateAssetMenuItems.cs
+++ Assets/Editor/CreateAssetMenuItems.cs
@@ -8,4 +8,7 @@ public static class CreateAssetMenuItems
 {
     [MenuItem("Assets/Create/Files/Text File")]
     public static void CreateEmptyTextAsset() => CreateAssetMenuItemsHelper.CreateAssetWithRename<TextAsset>("Create text file", "New Text File.txt", "");
+
+    [MenuItem("Assets/Create/Scripting/Completely Empty C# Script")]
+    public static void CreateCompletelyEmptyScript() => CreateAssetMenuItemsHelper.CreateAssetWithRename<MonoScript>("Create completely empty C# script", "New Script.cs", "");
 }
