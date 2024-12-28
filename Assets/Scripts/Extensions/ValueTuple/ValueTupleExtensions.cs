e5cd335c8ed9446635273ddcc3a3b82e5d2e1ae3
MrWoafer <woafer1000@gmail.com>
2024-12-28 20:52:04 +0000

    Simplify ValueTupleExtensions.Index()

--- Assets/Scripts/Extensions/ValueTuple/ValueTupleExtensions.cs
+++ Assets/Scripts/Extensions/ValueTuple/ValueTupleExtensions.cs
@@ -58,100 +58,79 @@ namespace PAC
         /// <summary>
         /// Returns the element at the given index in the tuple.
         /// </summary>
-        public static T Index<T>(this ValueTuple<T> valueTuple, int index)
+        public static T Index<T>(this ValueTuple<T> valueTuple, int index) => index switch
         {
-            switch (index)
-            {
-                case 0: return valueTuple.Item1;
-                default: throw new IndexOutOfRangeException("Tuple has 1 element, but the given index was " + index + ".");
-            }
-        }
+            0 => valueTuple.Item1,
+            _ => throw new IndexOutOfRangeException($"The tuple has 1 element, but the given index was {index}.")
+        };
         /// <summary>
         /// Returns the element at the given index in the tuple.
         /// </summary>
-        public static T Index<T>(this ValueTuple<T, T> valueTuple, int index)
+        public static T Index<T>(this ValueTuple<T, T> valueTuple, int index) => index switch
         {
-            switch (index)
-            {
-                case 0: return valueTuple.Item1;
-                case 1: return valueTuple.Item2;
-                default: throw new IndexOutOfRangeException("Tuple has 2 elements, but the given index was " + index + ".");
-            }
-        }
+            0 => valueTuple.Item1,
+            1 => valueTuple.Item2,
+            _ => throw new IndexOutOfRangeException($"The tuple has 2 elements, but the given index was {index}.")
+        };
         /// <summary>
         /// Returns the element at the given index in the tuple.
         /// </summary>
-        public static T Index<T>(this ValueTuple<T, T, T> valueTuple, int index)
+        public static T Index<T>(this ValueTuple<T, T, T> valueTuple, int index) => index switch
         {
-            switch (index)
-            {
-                case 0: return valueTuple.Item1;
-                case 1: return valueTuple.Item2;
-                case 2: return valueTuple.Item3;
-                default: throw new IndexOutOfRangeException("Tuple has 3 elements, but the given index was " + index + ".");
-            }
-        }
+            0 => valueTuple.Item1,
+            1 => valueTuple.Item2,
+            2 => valueTuple.Item3,
+            _ => throw new IndexOutOfRangeException($"The tuple has 3 elements, but the given index was {index}.")
+        };
         /// <summary>
         /// Returns the element at the given index in the tuple.
         /// </summary>
-        public static T Index<T>(this ValueTuple<T, T, T, T> valueTuple, int index)
+        public static T Index<T>(this ValueTuple<T, T, T, T> valueTuple, int index) => index switch
         {
-            switch (index)
-            {
-                case 0: return valueTuple.Item1;
-                case 1: return valueTuple.Item2;
-                case 2: return valueTuple.Item3;
-                case 3: return valueTuple.Item4;
-                default: throw new IndexOutOfRangeException("Tuple has 4 elements, but the given index was " + index + ".");
-            }
-        }
+            0 => valueTuple.Item1,
+            1 => valueTuple.Item2,
+            2 => valueTuple.Item3,
+            3 => valueTuple.Item4,
+            _ => throw new IndexOutOfRangeException($"The tuple has 4 elements, but the given index was {index}.")
+        };
         /// <summary>
         /// Returns the element at the given index in the tuple.
         /// </summary>
-        public static T Index<T>(this ValueTuple<T, T, T, T, T> valueTuple, int index)
+        public static T Index<T>(this ValueTuple<T, T, T, T, T> valueTuple, int index) => index switch
         {
-            switch (index)
-            {
-                case 0: return valueTuple.Item1;
-                case 1: return valueTuple.Item2;
-                case 2: return valueTuple.Item3;
-                case 3: return valueTuple.Item4;
-                case 4: return valueTuple.Item5;
-                default: throw new IndexOutOfRangeException("Tuple has 5 elements, but the given index was " + index + ".");
-            }
-        }
+            0 => valueTuple.Item1,
+            1 => valueTuple.Item2,
+            2 => valueTuple.Item3,
+            3 => valueTuple.Item4,
+            4 => valueTuple.Item5,
+            _ => throw new IndexOutOfRangeException($"The tuple has 5 elements, but the given index was {index}.")
+        };
         /// <summary>
         /// Returns the element at the given index in the tuple.
         /// </summary>
-        public static T Index<T>(this ValueTuple<T, T, T, T, T, T> valueTuple, int index)
+        public static T Index<T>(this ValueTuple<T, T, T, T, T, T> valueTuple, int index) => index switch
         {
-            switch (index)
-            {
-                case 0: return valueTuple.Item1;
-                case 1: return valueTuple.Item2;
-                case 2: return valueTuple.Item3;
-                case 3: return valueTuple.Item4;
-                case 4: return valueTuple.Item5;
-                case 5: return valueTuple.Item6;
-                default: throw new IndexOutOfRangeException("Tuple has 6 elements, but the given index was " + index + ".");
-            }
-        }
+            0 => valueTuple.Item1,
+            1 => valueTuple.Item2,
+            2 => valueTuple.Item3,
+            3 => valueTuple.Item4,
+            4 => valueTuple.Item5,
+            5 => valueTuple.Item6,
+            _ => throw new IndexOutOfRangeException($"The tuple has 6 elements, but the given index was {index}.")
+        };
         /// <summary>
         /// Returns the element at the given index in the tuple.
         /// </summary>
-        public static T Index<T>(this ValueTuple<T, T, T, T, T, T, T> valueTuple, int index)
+        public static T Index<T>(this ValueTuple<T, T, T, T, T, T, T> valueTuple, int index) => index switch
         {
-            switch (index)
-            {
-                case 0: return valueTuple.Item1;
-                case 1: return valueTuple.Item2;
-                case 2: return valueTuple.Item3;
-                case 3: return valueTuple.Item4;
-                case 4: return valueTuple.Item5;
-                case 5: return valueTuple.Item6;
-                case 6: return valueTuple.Item7;
-                default: throw new IndexOutOfRangeException("Tuple has 7 elements, but the given index was " + index + ".");
-            }
-        }
+            0 => valueTuple.Item1,
+            1 => valueTuple.Item2,
+            2 => valueTuple.Item3,
+            3 => valueTuple.Item4,
+            4 => valueTuple.Item5,
+            5 => valueTuple.Item6,
+            6 => valueTuple.Item7,
+            _ => throw new IndexOutOfRangeException($"The tuple has 7 elements, but the given index was {index}.")
+        };
     }
 }
