d33f7fd04481dd237bd2782bf811c3555e038897
MrWoafer <woafer1000@gmail.com>
2025-01-11 20:12:17 +0000

    Improve documentation for IRotatableShape

--- Assets/Scripts/Drawing/Shapes/Interfaces/IRotatableShape.cs
+++ Assets/Scripts/Drawing/Shapes/Interfaces/IRotatableShape.cs
@@ -2,41 +2,89 @@
 
 namespace PAC.Shapes
 {
+    /// <summary>
+    /// An <see cref="IShape"/> that can be rotated.
+    /// </summary>
     public interface IRotatableShape : IShape
     {
+        #region Contract
+        #region New Contract
+        /// <summary>
+        /// Rotates the shape by the given angle.
+        /// </summary>
+        /// <remarks>
+        /// This should return the same type as the object it was called from.
+        /// </remarks>
+        /// <returns>
+        /// A deep copy of the shape rotated by the given angle.
+        /// </returns>
+        public IRotatableShape Rotate(RotationAngle angle);
+        #endregion
+
+        #region Parent Contracts With More Derived Types
         /// <summary>
         /// Translates the shape by the given vector.
         /// </summary>
+        /// <remarks>
+        /// This should return the same type as the object it was called from.
+        /// </remarks>
+        /// <returns>
+        /// A deep copy of the shape translated by the given vector.
+        /// </returns>
+        /// <seealso cref="operator +(IShape, IntVector2)"/>
+        /// <seealso cref="operator +(IntVector2, IShape)"/>
+        /// <seealso cref="operator -(IShape, IntVector2)"/>
+        public new IRotatableShape Translate(IntVector2 translation);
+
+        /// <summary>
+        /// Reflects the shape across the given axis.
+        /// </summary>
+        /// <remarks>
+        /// This should return the same type as the object it was called from.
+        /// </remarks>
+        /// <returns>
+        /// A deep copy of the shape reflected across the given axis.
+        /// </returns>
+        /// <see cref="operator -(IShape)"/>
+        public new IRotatableShape Flip(FlipAxis axis);
+
+        public new IRotatableShape DeepCopy();
+        #endregion
+        #endregion
+
+        #region Default Implementations
+        /// <summary>
+        /// Translates the shape by the given vector.
+        /// </summary>
+        /// <returns>
+        /// A deep copy of the shape translated by the given vector.
+        /// </returns>
+        /// <seealso cref="Translate(IntVector2)"/>
         public static IRotatableShape operator +(IRotatableShape shape, IntVector2 translation) => shape.Translate(translation);
         /// <summary>
         /// Translates the shape by the given vector.
         /// </summary>
+        /// <returns>
+        /// A deep copy of the shape translated by the given vector.
+        /// </returns>
+        /// <seealso cref="Translate(IntVector2)"/>
         public static IRotatableShape operator +(IntVector2 translation, IRotatableShape shape) => shape + translation;
         /// <summary>
         /// Translates the shape by the given vector.
         /// </summary>
-        public static IRotatableShape operator -(IRotatableShape shape, IntVector2 translation) => shape + -translation;
+        /// <returns>
+        /// A deep copy of the shape translated by the given vector.
+        /// </returns>
+        /// <seealso cref="Translate(IntVector2)"/>
+        public static IRotatableShape operator -(IRotatableShape shape, IntVector2 translation) => shape + (-translation);
         /// <summary>
         /// Reflects the shape through the origin.
         /// </summary>
-        public static IRotatableShape operator -(IRotatableShape shape) => shape.Rotate(RotationAngle._180);
-
-        /// <summary>
-        /// Translates the shape by the given vector.
-        /// </summary>
-        public new IRotatableShape Translate(IntVector2 translation);
-        IShape IShape.Translate(IntVector2 translation) => Translate(translation);
-        /// <summary>
-        /// Reflects the shape across the given axis.
-        /// </summary>
-        public new IRotatableShape Flip(FlipAxis axis);
-        IShape IShape.Flip(FlipAxis axis) => Flip(axis);
-        /// <summary>
-        /// Rotates the shape by the given angle.
-        /// </summary>
-        public IRotatableShape Rotate(RotationAngle angle);
-
-        public new IRotatableShape DeepCopy();
-        IShape IShape.DeepCopy() => DeepCopy();
+        /// <returns>
+        /// A deep copy of the shape reflected through the origin.
+        /// </returns>
+        /// <seealso cref="Flip(FlipAxis)"/>
+        public static IRotatableShape operator -(IRotatableShape shape) => shape.Flip(FlipAxis.Vertical).Flip(FlipAxis.Horizontal);
+        #endregion
     }
 }
