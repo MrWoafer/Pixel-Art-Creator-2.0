bd12da62739aaadbab7264cddec46042f5eeaf54
MrWoafer <woafer1000@gmail.com>
2025-01-11 16:29:32 +0000

    Move IShapeExtensions into Extensions namespace

--- Assets/Scripts/Drawing/Toolbar.cs
+++ Assets/Scripts/Drawing/Toolbar.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using PAC.DataStructures;
+using PAC.Extensions;
 using PAC.Input;
 using PAC.KeyboardShortcuts;
 using PAC.Shapes;
---
+++ Assets/Scripts/Extensions/Shapes/IShapeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using PAC.DataStructures;
+using PAC.Shapes;
+
+using UnityEngine;
+
+namespace PAC.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="IShape"/>.
+    /// </summary>
+    public static class IShapeExtensions
+    {
+        /// <summary>
+        /// Turns the pixels in the shape's bounding rect into a Unity <see cref="Texture2D"/> with the given colour.
+        /// </summary>
+        public static Texture2D ToTexture(this IShape shape, Color colour) => shape.ToTexture(colour, shape.boundingRect);
+        /// <summary>
+        /// Turns the pixels in the given rect into a Unity <see cref="Texture2D"/> with the given colour, using any of the shape's points that lie within that rect. 
+        /// </summary>
+        public static Texture2D ToTexture(this IShape shape, Color colour, IntRect textureRect)
+        {
+            Texture2D texture = Tex2DSprite.BlankTexture(textureRect.width, textureRect.height);
+
+            foreach (IntVector2 pixel in shape)
+            {
+                if (textureRect.Contains(pixel))
+                {
+                    texture.SetPixel(pixel - textureRect.bottomLeft, colour);
+                }
+            }
+
+            texture.Apply();
+            return texture;
+        }
+    }
+}
