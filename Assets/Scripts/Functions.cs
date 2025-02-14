5ce83f86867c6d13725f96397b3f7243b2ee691c
MrWoafer <woafer1000@gmail.com>
2025-02-14 21:29:06 +0000

    Remove Functions.CompareArrays()

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -42,23 +42,5 @@ namespace PAC
 
             return copy;
         }
-
-        public static bool CompareArrays<T>(T[] array1, T[] array2)
-        {
-            if (array1.Length != array2.Length)
-            {
-                return false;
-            }
-
-            for (int i = 0; i < array1.Length; i++)
-            {
-                if (!array1[i].Equals(array2[i]))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
     }
 }
