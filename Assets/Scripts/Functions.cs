7bfd8a3de350f9d90c30bbcc2519ee61263bfc17
MrWoafer <woafer1000@gmail.com>
2025-02-14 21:12:26 +0000

    Remove Functions.ToArray()

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -78,12 +78,5 @@ namespace PAC
 
             return str;
         }
-
-        public static T[] ToArray<T>(this HashSet<T> hashSet)
-        {
-            T[] array = new T[hashSet.Count];
-            hashSet.CopyTo(array);
-            return array;
-        }
     }
 }
