d450d7edbcf31f94ceae85f2e5d1b4120ed2d236
MrWoafer <woafer1000@gmail.com>
2025-02-23 18:20:56 +0000

    Move ValueTupleExtensions into Extensions namespace

--- Assets/Scripts/Drawing/Shapes/Diamond.cs
+++ Assets/Scripts/Drawing/Shapes/Diamond.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using PAC.DataStructures;
+using PAC.Extensions;
 using PAC.Shapes.Interfaces;
 
 namespace PAC.Shapes
--- Assets/Scripts/Extensions/ValueTuple/ValueTupleExtensions.cs
+++ Assets/Scripts/Extensions/ValueTuple/ValueTupleExtensions.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace PAC
+namespace PAC.Extensions
 {
     public static class ValueTupleExtensions
     {
