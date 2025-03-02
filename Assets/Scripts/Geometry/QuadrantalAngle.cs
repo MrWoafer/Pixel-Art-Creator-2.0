7d618661274934ca3b286f3fa6c301fdaf8fb5a6
MrWoafer <woafer1000@gmail.com>
2025-03-02 11:30:25 +0000

    Change QuadrantalAngle underlying values

--- Assets/Scripts/Geometry/QuadrantalAngle.cs
+++ Assets/Scripts/Geometry/QuadrantalAngle.cs
@@ -3,11 +3,11 @@ namespace PAC.Geometry
     /// <summary>
     /// An angle that's a multiple of 90 degrees.
     /// </summary>
-    public enum QuadrantalAngle
+    public enum QuadrantalAngle : byte
     {
         _0 = 0,
-        Clockwise90 = 90,
-        Anticlockwise90 = -90,
-        _180 = 180
+        Clockwise90 = 1,
+        _180 = 2,
+        Anticlockwise90 = 3
     }
 }
