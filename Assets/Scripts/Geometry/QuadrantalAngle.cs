6057ad75181b679b0930f8360ae80d0815d07126
MrWoafer <woafer1000@gmail.com>
2025-03-03 22:39:54 +0000

    Comment QuadrantalAngle values

--- Assets/Scripts/Geometry/QuadrantalAngle.cs
+++ Assets/Scripts/Geometry/QuadrantalAngle.cs
@@ -5,9 +5,21 @@ namespace PAC.Geometry
     /// </summary>
     public enum QuadrantalAngle : byte
     {
+        /// <summary>
+        /// 0 degrees.
+        /// </summary>
         _0 = 0,
+        /// <summary>
+        /// 90 degrees in the clockwise direction.
+        /// </summary>
         Clockwise90 = 1,
+        /// <summary>
+        /// 180 degrees.
+        /// </summary>
         _180 = 2,
+        /// <summary>
+        /// 90 degrees in the anticlockwise direction.
+        /// </summary>
         Anticlockwise90 = 3
     }
 }
