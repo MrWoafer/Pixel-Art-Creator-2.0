894489dfdf397f2d3980882873098417e93dea6e
MrWoafer <woafer1000@gmail.com>
2025-03-22 15:15:47 +0000

    Add conversion tests

---
+++ Assets/Tests/Colour/RGB_Tests.cs
@@ -0,0 +1,129 @@
+using System;
+
+using NUnit.Framework;
+
+using PAC.Colour;
+using PAC.Extensions;
+
+namespace PAC.Tests.Colour
+{
+    /// <summary>
+    /// Tests for <see cref="RGB"/>.
+    /// </summary>
+    public class RGB_Tests
+    {
+        /// <summary>
+        /// Tests converting <see cref="RGB"/> to <see cref="HSL"/> with some examples from <see href="https://en.wikipedia.org/wiki/HSL_and_HSV#Examples"/>.
+        /// </summary>
+        [Test]
+        [Category("Colour")]
+        public void CastTo_HSL_Examples()
+        {
+            (RGB input, HSL expected)[] testCases =
+            {
+                (new RGB(1f, 0f, 0f), new HSL(0f, 1f, 0.5f)),
+                (new RGB(0f, 0.5f, 0f), new HSL(120f / 360f, 1f, 0.25f)),
+                (new RGB(0.704f, 0.187f, 0.897f), new HSL(283.7f / 360f, 0.775f, 0.542f)),
+                (new RGB(0.099f, 0.795f,  0.591f), new HSL(162.4f / 360f, 0.779f, 0.447f)),
+                (new RGB(0.495f, 0.493f, 0.721f), new HSL(240.5f / 360f, 0.290f, 0.607f)),
+            };
+
+            foreach ((RGB input, HSL expected) in testCases)
+            {
+                HSL observed = (HSL)input;
+                Assert.True(expected.Equals(observed, 0.001f), $"Failed with {input}.\nExpected: {expected}\nObserved: {observed}");
+            }
+        }
+
+        /// <summary>
+        /// Tests that <c>RGB(x, x, x)</c> converts to <c>HSL(*, 0, x)</c>.
+        /// </summary>
+        [Test]
+        [Category("Colour")]
+        public void CastTo_HSL_Greyscale()
+        {
+            Random random = new Random(TestContext.CurrentContext.Test.Name.GetHashCode());
+            for (int iteration = 0; iteration < 1_000; iteration++)
+            {
+                float x = random.NextFloat();
+                RGB input = new RGB(x, x, x);
+                HSL observed = (HSL)input;
+                HSL expected = new HSL(observed.h, 0f, x);
+                Assert.True(expected.Equals(observed, 0.001f), $"Failed with {input}.\nExpected: {expected}\nObserved: {observed}");
+            }
+        }
+
+        /// <summary>
+        /// Tests that converting <see cref="RGB"/> to <see cref="HSL"/> and then back to <see cref="RGB"/> gives the original colour.
+        /// </summary>
+        [Test]
+        [Category("Colour")]
+        public void CastTo_HSL_AndBackIsIdentity()
+        {
+            Random random = new Random(TestContext.CurrentContext.Test.Name.GetHashCode());
+            for (int iteration = 0; iteration < 1_000; iteration++)
+            {
+                RGB input = random.NextRGB();
+                RGB observed = (RGB)(HSL)input;
+                Assert.True(input.Equals(observed, 0.001f), $"Failed with {input}.\nExpected: {input}\nObserved: {observed}");
+            }
+        }
+
+        /// <summary>
+        /// Tests converting <see cref="RGB"/> to <see cref="HSV"/> with some examples from <see href="https://en.wikipedia.org/wiki/HSL_and_HSV#Examples"/>.
+        /// </summary>
+        [Test]
+        [Category("Colour")]
+        public void CastTo_HSV_Examples()
+        {
+            (RGB input, HSV expected)[] testCases =
+            {
+                (new RGB(1f, 0f, 0f), new HSV(0f, 1f, 1f)),
+                (new RGB(0f, 0.5f, 0f), new HSV(120f / 360f, 1f, 0.5f)),
+                (new RGB(0.704f, 0.187f, 0.897f), new HSV(283.7f / 360f, 0.792f, 0.897f)),
+                (new RGB(0.099f, 0.795f,  0.591f), new HSV(162.4f / 360f, 0.875f, 0.795f)),
+                (new RGB(0.495f, 0.493f, 0.721f), new HSV(240.5f / 360f, 0.316f, 0.721f)),
+            };
+
+            foreach ((RGB input, HSV expected) in testCases)
+            {
+                HSV observed = (HSV)input;
+                Assert.True(expected.Equals(observed, 0.001f), $"Failed with {input}.\nExpected: {expected}\nObserved: {observed}");
+            }
+        }
+
+        /// <summary>
+        /// Tests that <c>RGB(x, x, x)</c> converts to <c>HSV(*, 0, x)</c>.
+        /// </summary>
+        [Test]
+        [Category("Colour")]
+        public void CastTo_HSV_Greyscale()
+        {
+            Random random = new Random(TestContext.CurrentContext.Test.Name.GetHashCode());
+            for (int iteration = 0; iteration < 1_000; iteration++)
+            {
+                float x = random.NextFloat();
+                RGB input = new RGB(x, x, x);
+                HSV observed = (HSV)input;
+                HSV expected = new HSV(observed.h, 0f, x);
+                Assert.True(expected.Equals(observed, 0.001f), $"Failed with {input}.\nExpected: {expected}\nObserved: {observed}");
+            }
+        }
+
+        /// <summary>
+        /// Tests that converting <see cref="RGB"/> to <see cref="HSV"/> and then back to <see cref="RGB"/> gives the original colour.
+        /// </summary>
+        [Test]
+        [Category("Colour")]
+        public void CastTo_HSV_AndBackIsIdentity()
+        {
+            Random random = new Random(TestContext.CurrentContext.Test.Name.GetHashCode());
+            for (int iteration = 0; iteration < 1_000; iteration++)
+            {
+                RGB input = random.NextRGB();
+                RGB observed = (RGB)(HSV)input;
+                Assert.True(input.Equals(observed, 0.001f), $"Failed with {input}.\nExpected: {input}\nObserved: {observed}");
+            }
+        }
+    }
+}
