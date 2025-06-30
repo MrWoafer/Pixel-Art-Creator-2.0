0ad3476b09620a2b23f428d2d81d337bcc3f94bc
MrWoafer <woafer1000@gmail.com>
2025-06-30 22:53:51 +0100

    Fix IFlippableShape_RequiredTests comment link

--- Assets/Tests/Geometry/Shapes/Required Tests/IFlippableShape_RequiredTests.cs
+++ Assets/Tests/Geometry/Shapes/Required Tests/IFlippableShape_RequiredTests.cs
@@ -1,12 +1,14 @@
+using PAC.Geometry.Shapes.Interfaces;
+
 namespace PAC.Tests.Geometry.Shapes.RequiredTests
 {
     /// <summary>
-    /// Describes tests that should be implemented for every implementation of <see cref="IFlippableShape{T}"/>.
+    /// Describes tests that should be implemented for every implementation of <see cref="IFlippableShape{T, A}"/>.
     /// </summary>
     public interface IFlippableShape_RequiredTests : IDeepCopyableShape_RequiredTests
     {
         /// <summary>
-        /// Tests <see cref="IFlippableShape{T}.Flip(CardinalOrdinalAxis)"/>.
+        /// Tests <see cref="IFlippableShape{T, A}.Flip(A)"/>.
         /// </summary>
         public void Flip();
     }
