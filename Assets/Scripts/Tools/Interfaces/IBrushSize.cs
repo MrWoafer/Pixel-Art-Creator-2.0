0bac51f6ded358c6f4c44081b3c4bb96e39724e2
MrWoafer <woafer1000@gmail.com>
2025-07-23 22:38:24 +0100

    Add IBrushSize

---
+++ Assets/Scripts/Tools/Interfaces/IBrushSize.cs
@@ -0,0 +1,10 @@
+namespace PAC.Tools.Interfaces
+{
+    /// <summary>
+    /// A <see cref="Tool"/> that has a configurable brush size.
+    /// </summary>
+    public interface IBrushSize
+    {
+        public int brushSize { get; set; }
+    }
+}
